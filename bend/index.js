6f962d65aa40d927556e6ecfcad6c78d4d238753
Kevins Villatoro <kevinnz36@gmail.com>
2024-03-17 22:01:05 -0300

    modificación server para peticiones

--- bend/index.js
+++ bend/index.js
@@ -6,15 +6,13 @@ const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
+  methods: ['GET', 'POST']
+}));
 
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
-        methods: ['GET', 'POST']
-    }
-});
+const io = new Server(server);
 
 io.on('connection', (socket) => {
     console.log(`Usuario actual: ${socket.id}`);
